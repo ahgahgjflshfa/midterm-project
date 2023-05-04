7fb47a696c7f17990ab66cd8396eb047018afdd7
ahgahg <aaa12345.59487@gamil.com>
2023-05-04 14:59:55 +0800

    add slt function to alu

--- ALU.v
+++ ALU.v
@@ -8,45 +8,53 @@
 		1. result: 最後處理完的結果
 		2. zero:   branch指令所需要之輸出
 */
-module Alu(ctl, a, b, result, zero);
+module Alu(ctl, a, b, result);
     input [5:0] ctl;
     input [31:0] a, b;
-    output reg [31:0] result;
-    output reg zero;
+    output wire [31:0] result;
+    output carry;
 
     wire inv, cout;
-    assign inv = (ctl == 34) ? 1 : 0;
-    One_bit_alu_slice aluslice0(.ctl(ctl), .ai(a[0]), .bi(b[0]), .invb(inv),.cin(inv), .sum(result[0]), .cout(cout));
-    One_bit_alu_slice aluslice1(.ctl(ctl), .ai(a[1]), .bi(b[1]), .invb(inv),.cin(cout), .sum(result[1]), .cout(cout));
-    One_bit_alu_slice aluslice2(.ctl(ctl), .ai(a[2]), .bi(b[2]), .invb(inv),.cin(cout), .sum(result[2]), .cout(cout));
-    One_bit_alu_slice aluslice3(.ctl(ctl), .ai(a[3]), .bi(b[3]), .invb(inv),.cin(cout), .sum(result[3]), .cout(cout));
-    One_bit_alu_slice aluslice4(.ctl(ctl), .ai(a[4]), .bi(b[4]), .invb(inv),.cin(cout), .sum(result[4]), .cout(cout));
-    One_bit_alu_slice aluslice5(.ctl(ctl), .ai(a[5]), .bi(b[5]), .invb(inv),.cin(cout), .sum(result[5]), .cout(cout));
-    One_bit_alu_slice aluslice6(.ctl(ctl), .ai(a[6]), .bi(b[6]), .invb(inv),.cin(cout), .sum(result[6]), .cout(cout));
-    One_bit_alu_slice aluslice7(.ctl(ctl), .ai(a[7]), .bi(b[7]), .invb(inv),.cin(cout), .sum(result[7]), .cout(cout));
-    One_bit_alu_slice aluslice8(.ctl(ctl), .ai(a[8]), .bi(b[8]), .invb(inv),.cin(cout), .sum(result[8]), .cout(cout));
-    One_bit_alu_slice aluslice9(.ctl(ctl), .ai(a[9]), .bi(b[9]), .invb(inv),.cin(cout), .sum(result[9]), .cout(cout));
-    One_bit_alu_slice aluslice10(.ctl(ctl), .ai(a[10]), .bi(b[10]), .invb(inv),.cin(cout), .sum(result[10]), .cout(cout));
-    One_bit_alu_slice aluslice11(.ctl(ctl), .ai(a[11]), .bi(b[11]), .invb(inv),.cin(cout), .sum(result[11]), .cout(cout));
-    One_bit_alu_slice aluslice12(.ctl(ctl), .ai(a[12]), .bi(b[12]), .invb(inv),.cin(cout), .sum(result[12]), .cout(cout));
-    One_bit_alu_slice aluslice13(.ctl(ctl), .ai(a[13]), .bi(b[13]), .invb(inv),.cin(cout), .sum(result[13]), .cout(cout));
-    One_bit_alu_slice aluslice14(.ctl(ctl), .ai(a[14]), .bi(b[14]), .invb(inv),.cin(cout), .sum(result[14]), .cout(cout));
-    One_bit_alu_slice aluslice15(.ctl(ctl), .ai(a[15]), .bi(b[15]), .invb(inv),.cin(cout), .sum(result[15]), .cout(cout));
-    One_bit_alu_slice aluslice16(.ctl(ctl), .ai(a[16]), .bi(b[16]), .invb(inv),.cin(cout), .sum(result[16]), .cout(cout));
-    One_bit_alu_slice aluslice17(.ctl(ctl), .ai(a[17]), .bi(b[17]), .invb(inv),.cin(cout), .sum(result[17]), .cout(cout));
-    One_bit_alu_slice aluslice18(.ctl(ctl), .ai(a[18]), .bi(b[18]), .invb(inv),.cin(cout), .sum(result[18]), .cout(cout));
-    One_bit_alu_slice aluslice19(.ctl(ctl), .ai(a[19]), .bi(b[19]), .invb(inv),.cin(cout), .sum(result[19]), .cout(cout));
-    One_bit_alu_slice aluslice20(.ctl(ctl), .ai(a[20]), .bi(b[20]), .invb(inv),.cin(cout), .sum(result[20]), .cout(cout));
-    One_bit_alu_slice aluslice21(.ctl(ctl), .ai(a[21]), .bi(b[21]), .invb(inv),.cin(cout), .sum(result[21]), .cout(cout));
-    One_bit_alu_slice aluslice22(.ctl(ctl), .ai(a[22]), .bi(b[22]), .invb(inv),.cin(cout), .sum(result[22]), .cout(cout));
-    One_bit_alu_slice aluslice23(.ctl(ctl), .ai(a[23]), .bi(b[23]), .invb(inv),.cin(cout), .sum(result[23]), .cout(cout));
-    One_bit_alu_slice aluslice24(.ctl(ctl), .ai(a[24]), .bi(b[24]), .invb(inv),.cin(cout), .sum(result[24]), .cout(cout));
-    One_bit_alu_slice aluslice25(.ctl(ctl), .ai(a[25]), .bi(b[25]), .invb(inv),.cin(cout), .sum(result[25]), .cout(cout));
-    One_bit_alu_slice aluslice26(.ctl(ctl), .ai(a[26]), .bi(b[26]), .invb(inv),.cin(cout), .sum(result[26]), .cout(cout));
-    One_bit_alu_slice aluslice27(.ctl(ctl), .ai(a[27]), .bi(b[27]), .invb(inv),.cin(cout), .sum(result[27]), .cout(cout));
-    One_bit_alu_slice aluslice28(.ctl(ctl), .ai(a[28]), .bi(b[28]), .invb(inv),.cin(cout), .sum(result[28]), .cout(cout));
-    One_bit_alu_slice aluslice29(.ctl(ctl), .ai(a[29]), .bi(b[29]), .invb(inv),.cin(cout), .sum(result[29]), .cout(cout));
-    One_bit_alu_slice aluslice30(.ctl(ctl), .ai(a[30]), .bi(b[30]), .invb(inv),.cin(cout), .sum(result[30]), .cout(cout));
-    One_bit_alu_slice aluslice31(.ctl(ctl), .ai(a[31]), .bi(b[31]), .invb(inv),.cin(cout), .sum(result[31]), .cout(cout));
+    assign inv = (ctl == 34) ? 1'b1:   // sub
+                 (ctl == 42) ? 1'b1:   // slt
+                               1'b0;   // other
+
+    wire [31:0] sum;
+
+    One_bit_alu_slice aluslice0(.ctl(ctl), .ai(a[0]), .bi(b[0]), .invb(inv),.cin(0), .sum(sum[0]), .cout(cout));
+    One_bit_alu_slice aluslice1(.ctl(ctl), .ai(a[1]), .bi(b[1]), .invb(inv),.cin(cout), .sum(sum[1]), .cout(cout));
+    One_bit_alu_slice aluslice2(.ctl(ctl), .ai(a[2]), .bi(b[2]), .invb(inv),.cin(cout), .sum(sum[2]), .cout(cout));
+    One_bit_alu_slice aluslice3(.ctl(ctl), .ai(a[3]), .bi(b[3]), .invb(inv),.cin(cout), .sum(sum[3]), .cout(cout));
+    One_bit_alu_slice aluslice4(.ctl(ctl), .ai(a[4]), .bi(b[4]), .invb(inv),.cin(cout), .sum(sum[4]), .cout(cout));
+    One_bit_alu_slice aluslice5(.ctl(ctl), .ai(a[5]), .bi(b[5]), .invb(inv),.cin(cout), .sum(sum[5]), .cout(cout));
+    One_bit_alu_slice aluslice6(.ctl(ctl), .ai(a[6]), .bi(b[6]), .invb(inv),.cin(cout), .sum(sum[6]), .cout(cout));
+    One_bit_alu_slice aluslice7(.ctl(ctl), .ai(a[7]), .bi(b[7]), .invb(inv),.cin(cout), .sum(sum[7]), .cout(cout));
+    One_bit_alu_slice aluslice8(.ctl(ctl), .ai(a[8]), .bi(b[8]), .invb(inv),.cin(cout), .sum(sum[8]), .cout(cout));
+    One_bit_alu_slice aluslice9(.ctl(ctl), .ai(a[9]), .bi(b[9]), .invb(inv),.cin(cout), .sum(sum[9]), .cout(cout));
+    One_bit_alu_slice aluslice10(.ctl(ctl), .ai(a[10]), .bi(b[10]), .invb(inv),.cin(cout), .sum(sum[10]), .cout(cout));
+    One_bit_alu_slice aluslice11(.ctl(ctl), .ai(a[11]), .bi(b[11]), .invb(inv),.cin(cout), .sum(sum[11]), .cout(cout));
+    One_bit_alu_slice aluslice12(.ctl(ctl), .ai(a[12]), .bi(b[12]), .invb(inv),.cin(cout), .sum(sum[12]), .cout(cout));
+    One_bit_alu_slice aluslice13(.ctl(ctl), .ai(a[13]), .bi(b[13]), .invb(inv),.cin(cout), .sum(sum[13]), .cout(cout));
+    One_bit_alu_slice aluslice14(.ctl(ctl), .ai(a[14]), .bi(b[14]), .invb(inv),.cin(cout), .sum(sum[14]), .cout(cout));
+    One_bit_alu_slice aluslice15(.ctl(ctl), .ai(a[15]), .bi(b[15]), .invb(inv),.cin(cout), .sum(sum[15]), .cout(cout));
+    One_bit_alu_slice aluslice16(.ctl(ctl), .ai(a[16]), .bi(b[16]), .invb(inv),.cin(cout), .sum(sum[16]), .cout(cout));
+    One_bit_alu_slice aluslice17(.ctl(ctl), .ai(a[17]), .bi(b[17]), .invb(inv),.cin(cout), .sum(sum[17]), .cout(cout));
+    One_bit_alu_slice aluslice18(.ctl(ctl), .ai(a[18]), .bi(b[18]), .invb(inv),.cin(cout), .sum(sum[18]), .cout(cout));
+    One_bit_alu_slice aluslice19(.ctl(ctl), .ai(a[19]), .bi(b[19]), .invb(inv),.cin(cout), .sum(sum[19]), .cout(cout));
+    One_bit_alu_slice aluslice20(.ctl(ctl), .ai(a[20]), .bi(b[20]), .invb(inv),.cin(cout), .sum(sum[20]), .cout(cout));
+    One_bit_alu_slice aluslice21(.ctl(ctl), .ai(a[21]), .bi(b[21]), .invb(inv),.cin(cout), .sum(sum[21]), .cout(cout));
+    One_bit_alu_slice aluslice22(.ctl(ctl), .ai(a[22]), .bi(b[22]), .invb(inv),.cin(cout), .sum(sum[22]), .cout(cout));
+    One_bit_alu_slice aluslice23(.ctl(ctl), .ai(a[23]), .bi(b[23]), .invb(inv),.cin(cout), .sum(sum[23]), .cout(cout));
+    One_bit_alu_slice aluslice24(.ctl(ctl), .ai(a[24]), .bi(b[24]), .invb(inv),.cin(cout), .sum(sum[24]), .cout(cout));
+    One_bit_alu_slice aluslice25(.ctl(ctl), .ai(a[25]), .bi(b[25]), .invb(inv),.cin(cout), .sum(sum[25]), .cout(cout));
+    One_bit_alu_slice aluslice26(.ctl(ctl), .ai(a[26]), .bi(b[26]), .invb(inv),.cin(cout), .sum(sum[26]), .cout(cout));
+    One_bit_alu_slice aluslice27(.ctl(ctl), .ai(a[27]), .bi(b[27]), .invb(inv),.cin(cout), .sum(sum[27]), .cout(cout));
+    One_bit_alu_slice aluslice28(.ctl(ctl), .ai(a[28]), .bi(b[28]), .invb(inv),.cin(cout), .sum(sum[28]), .cout(cout));
+    One_bit_alu_slice aluslice29(.ctl(ctl), .ai(a[29]), .bi(b[29]), .invb(inv),.cin(cout), .sum(sum[29]), .cout(cout));
+    One_bit_alu_slice aluslice30(.ctl(ctl), .ai(a[30]), .bi(b[30]), .invb(inv),.cin(cout), .sum(sum[30]), .cout(cout));
+    One_bit_alu_slice aluslice31(.ctl(ctl), .ai(a[31]), .bi(b[31]), .invb(inv),.cin(cout), .sum(sum[31]), .cout(cout));
+
+    assign result = (ctl == 42) ? {31'b0, sum[31]}: // slt
+                                  sum;              // other
 
 endmodule
--- One_bit_ALU_slice.v
+++ One_bit_ALU_slice.v
@@ -1,7 +1,7 @@
 /*
     Title: 1 bit alu bit slice
     Input port:
-        1. sel: 功能選擇
+        1. ctl: 功能選擇
         2. ai: 被加數
         3. bi: 加數
         4. invb: invert b (減法)
@@ -10,10 +10,10 @@
         1. sum: 結果
         2. cout: carry out
 */
-module One_bit_alu_slice(ctl, ai, bi, invb, cin, sum, cout);
+module One_bit_alu_slice(ctl, ai, bi, invb, cin, sum, cout, set);
     input [5:0] ctl;
     input ai, bi, invb, cin;
-    output sum, cout;
+    output sum, cout, set;
 
     wire temp0, temp1, temp2;
     // and
@@ -27,12 +27,15 @@ module One_bit_alu_slice(ctl, ai, bi, invb, cin, sum, cout);
     xor(e1, b, invb);
     Full_adder fa(.a(ai), .b(e1), .cin(cin), .sum(temp2), .cout(cout));
 
+    // assign set
+    assign set = temp2;
+
     // assign sel
     wire sel;
     assign sel = (ctl == 36) ? 2'b00:   // and
                  (ctl == 37) ? 2'b01:   // or
-                 (ctl == 32) ? 2'b00:   // add
-                               2'b00;   // sub
+                 (ctl == 32) ? 2'b10:   // add
+                               2'b10;   // sub and slt
 
     // select
     Mux_4to1 mux41(.sel(sel), .in0(temp0), .in1(temp1), .in2(temp2), .out(sum));
