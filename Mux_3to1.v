45455383d632c361fb60df1e976c0092577892ba
ahgahg <aaa12345.59487@gamil.com>
2023-05-04 13:09:08 +0800

    delete mux 3 to 1

--- Mux_3to1.v
+++
@@ -1,5 +0,0 @@
-/*
-    Title: 3 to 1 Mux
-*/
-module Mux_3to1(sel, in0, in1, in2);
-endmodule
--- Mux_4to1.v
+++ Mux_4to1.v
@@ -6,6 +6,6 @@ module Mux_4to1(sel, in0, in1, in2, in3, out);
     assign out = (sel == 2'b00) ? in0:
                  (sel == 2'b01) ? in1:
                  (sel == 2'b10) ? in2:
-                                      in3;
+                                  in3;
 
 endmodule
