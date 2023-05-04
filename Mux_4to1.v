d917e7522db958f58ff688e42faaf8a27a915087
ahgahg <aaa12345.59487@gamil.com>
2023-05-04 13:08:07 +0800

    Fix mux 4 to 1

--- Mux_4to1.v
+++ Mux_4to1.v
@@ -1,11 +1,11 @@
-module Mux_4to1(sel, in0, in1, in2, in3, out)
+module Mux_4to1(sel, in0, in1, in2, in3, out);
     input sel;
     input in0, in1, in2, in3;
-    output out;
+    output wire out;
 
-    assign out = (aluctrl == 2'b00) ? in0:
-                 (aluctrl == 2'b01) ? in1:
-                 (aluctrl == 2'b10) ? in2:
+    assign out = (sel == 2'b00) ? in0:
+                 (sel == 2'b01) ? in1:
+                 (sel == 2'b10) ? in2:
                                       in3;
 
 endmodule
